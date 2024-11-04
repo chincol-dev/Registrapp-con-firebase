7bb32a975d04b136d1311a34b20498aeff9680bd
JeanPiffaut <piffaut.jean@gmail.com>
2024-11-04 00:59:18 -0300

    creando componente de listado de clases

---
+++ src/app/clases-list/clases-list.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-clases-list',
+  templateUrl: './clases-list.component.html',
+  styleUrls: ['./clases-list.component.scss'],
+})
+export class ClasesListComponent  implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
