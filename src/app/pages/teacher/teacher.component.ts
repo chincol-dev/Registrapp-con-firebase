041eae8fe0f596f07b00affa56accbcb2f18c1cd
JeanPiffaut <piffaut.jean@gmail.com>
2024-11-04 02:10:48 -0300

    listado de clases de profesores

--- src/app/pages/teacher/teacher.component.ts
+++ src/app/pages/teacher/teacher.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { FirebaseService } from 'src/app/services/firebase.service';
 
 @Component({
   selector: 'app-teacher',
@@ -7,14 +6,9 @@ import { FirebaseService } from 'src/app/services/firebase.service';
   styleUrls: ['./teacher.component.scss']
 })
 export class TeacherComponent implements OnInit {
-  qrCodeValue: string;
-
-  constructor(private firebaseService: FirebaseService) { }
+  constructor() { }
 
   ngOnInit(): void {
-    // Asignar el valor del QR directamente
-    this.qrCodeValue = 'https://example.com';
-
     // Aquí puedes agregar lógica adicional si es necesario
     console.log('TeacherComponent inicializado, listo para mostrar la lista de clases.');
   }
